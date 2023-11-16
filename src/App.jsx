267c7b6d51a4a7c793d0b4d217b71514dfd2a495
刘文强25704 <liuwq25704@yunrong.cn>
2023-11-16 17:31:18 +0800

    init,2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import {Menu} from 'antd';
+import 'antd/dist/antd.css';
 import './App.css'
 import MouseTracker from "./RecordXDistance/RecordXDistanceIndex";
+
+const items = [
+  { label: '菜单项一', key: 'item-1' }, // 菜单项务必填写 key
+  { label: '菜单项二', key: 'item-2' },
+  {
+    label: '子菜单',
+    key: 'submenu',
+    children: [{ label: '子菜单项', key: 'submenu-item-1' }],
+  },
+];
 
 function App() {
   return (
     <section className="page">
       <aside className="aside">
-
+        <Menu items={items} />
       </aside>
       <main className="main">
         <MouseTracker />
